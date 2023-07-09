5ef765dc6f0cc5b839586a14929f86a99dcd7d51
SHEKHAR <137384631+shekhar-sg@users.noreply.github.com>
2023-07-09 15:49:53 +0530

    :update, 1. Initial setup, 2. TailwindCss added

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import TodoList from '@/src/components/todo-list'
+
+const Home = () => {
+  // const {todos} = todoService
+  return (
+    <>
+      <TodoList />
+    </>
+  )
+}
---
+++ src/components/todo-list.tsx
@@ -0,0 +1,11 @@
+import TodoItem from "@/src/components/todo-item";
+
+const TodoList = () => {
+    return (
+        <div>
+            <TodoItem/>
+        </div>
+    );
+};
+
+export default TodoList;
